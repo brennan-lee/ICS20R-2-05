dba14b2fec6ae80d841f1661de8a3ff0013689bf
brennan-lee <brennan-lee@users.noreply.github.com>
2022-10-28 18:21:23 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -17,5 +17,7 @@ function calculate() {
 
   document.getElementById("take").innerHTML =
     "The governemnt will take: " + take + "$"
-    document.getElementById('wage').innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
-  }
+  document.getElementById(
+    "wage"
+  ).innerHTML = `Your pay will be: $ ${takeHomeSalary.toFixed(2)}`
+}
