026dabba3693c5d9c5e9150e2ac4f94424e9297d
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-10-31 13:44:33 -0400

    js

--- js/script.js
+++ js/script.js
@@ -7,15 +7,12 @@
 "use strict"
 
 function calculate() {
-  const hours = parseInt(
-    document.getElementById("number-of-hours-worked").value
-  )
+  const hours = parseInt(document.getElementById("number-of-hours-worked").value)
   const rate = parseInt(document.getElementById("Hourly-rate").value)
 
   const take = hours * rate * 0.18
   const wage = hours * rate * (1.0 - 0.18)
 
-  document.getElementById("take").innerHTML = "${take.toFixed(2)}"
-  "The governemnt will take: " + take + "$"
+  document.getElementById("take").innerHTML = "The governemnt will take: " + take + "$"
   document.getElementById("wage").innerHTML = "your pay will be: " + wage + "$"
 }
